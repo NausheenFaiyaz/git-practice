7f3265623a14cbe3abb0b26883a52e7ad7016ae0
Nausheen Faiyaz <logme2nausheen@gmail.com>
2025-05-21 21:52:21 +0530

    Added isAbove fn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const age = 21;
-const name = "Nausheen Faiyaz";
+const name1 = "Nausheen Faiyaz";
 const dob = "29/05/2004";
 
 function sayName() {
-  console.log("My Name is " + name);
+  console.log("My Name is " + name1);
+}
+
+function isAbove18() {
+  return age >= 18;
 }
